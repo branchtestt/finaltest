993301a149cc1d827d04295e73e637e9e4c09614
rajhere <160505876+ItsmeRAAAJ@users.noreply.github.com>
2025-07-19 18:37:52 +0000

    made somechanges, now its automatically redeploying

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react"
+import axios from "axios";
+
 export default function Home() {
+
+  const [joke , setJoke] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("https://api-playapi.vercel.app/api/v1/jokes/random");
+
+      setJoke(response.data.content);      
+
+    }
+    fetchData();
+  } , [])
+
   return (
-    <div className="text-center bg-red-300">
-      hello world
+    <div className="text-center flex min-h-screen justify-center items-center p-5 font-extrabold md:text-4xl text-xl bg-cyan-300">
+      <div>
+        {joke}
+      </div>
     </div>
   )
 }
